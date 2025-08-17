5c3b8d6d841484f2f129d2134eed65adbab68315
Shadowedvaca <shadowedvaca@gmail.com>
2025-08-16 22:40:54 -0700

    more testing

--- start_session.ps1
+++ start_session.ps1
@@ -1,6 +1,6 @@
 ﻿#Normal
-uvicorn app.main:app --reload --host 0.0.0.0 --port 8000got
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
 # W/ Verbose Logs
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --log-level debug
 # W/O reloader and with access logs
-uvicorn app.main:app --host 127.0.0.1 --port 8000 --log-level debug --access-log
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-level debug --access-log
